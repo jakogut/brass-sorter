abde6681530f95c0b753ab54056b94218b63cf75
Joseph Kogut <joseph.kogut@gmail.com>
2018-02-27 20:00:13 -0800

    initial commit

---
+++ predict.py
@@ -0,0 +1,37 @@
+'''
+Loads a trained model, and classifies an image
+
+argv[1]: path to hdf5 model to load
+argv[2]: path to image to classify
+'''
+
+from keras.models import load_model
+from keras.preprocessing.image import load_img, img_to_array, array_to_img
+
+import sys
+import time
+import numpy as np
+
+model = load_model(sys.argv[1])
+img = load_img(sys.argv[2], target_size=(72, 72))
+x = img_to_array(img)
+x = np.expand_dims(x, axis=0)
+
+start_time = time.time()
+prediction = model.predict(x, verbose=1)[0][0]
+end_time = time.time()
+
+labels = ['LC', 'NLC']
+rounded = int(round(prediction))
+result = labels[rounded]
+
+old_range = 0.5
+new_range = 100.0
+
+if rounded:
+    pct = ((prediction - 0.5) * 100.0) / 0.5
+else:
+    pct = 100.0 - (prediction * 100.0 / 0.5)
+
+
+print(f'elapsed time: {end_time - start_time}s, prediction: {result}, certainty: {pct}%')
---
+++ train.py
@@ -0,0 +1,69 @@
+from keras.models import Sequential
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, ZeroPadding2D, Activation
+from keras.optimizers import Adam
+
+from keras.preprocessing.image import ImageDataGenerator
+
+input_size = 72, 72
+input_shape = *input_size, 3
+num_classes = 1
+
+def create_small():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    return model
+
+model = create_small()
+model.summary()
+
+batch_size = 256
+epochs = 100
+
+train_datagen = ImageDataGenerator(
+    rescale=1.0,
+    rotation_range=180,
+    fill_mode='nearest',
+)
+
+test_datagen = ImageDataGenerator(rescale=0.25)
+
+train_generator = test_datagen.flow_from_directory(
+    'data/train', target_size=input_size, batch_size=32, class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    'data/validation', target_size=input_size, batch_size=32, class_mode='binary') 
+
+early_stop = EarlyStopping(monitor='val_acc', min_delta=0.0001, patience=10,
+        verbose=1, mode='auto')
+
+checkpointer = ModelCheckpoint(filepath='brass_classifier.hdf5', verbose=1, save_best_only=True)
+callbacks = [early_stop, checkpointer]
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=250,
+    epochs=50,
+    validation_data=validation_generator,
+    validation_steps=800,
+    callbacks=callbacks
+)
